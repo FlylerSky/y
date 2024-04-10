a5545625dd8053c4dc41c6ea556d6b8fce4baa14
roesuryvoe@gmail.com <roesuryvoe@gmail.com>
2024-04-10 15:29:54 +0700

    HELLO

---
+++ Trang Web/GHOST/ghost.js
@@ -0,0 +1,1 @@
+//alert('CẢNH BÁO: TRUYỆN MA KHÔNG ĐẢM BẢO CHO LỨA TUỔI DƯỚI 18!_VUI LÒNG CÂN NHẮC')
---
+++ main.js
@@ -0,0 +1,4 @@
+alert('CHÀO MỪNG 🐜');
+git remote add origin https: //github.com/FlylerSky/fly.git
+  git branch - M main
+git push - u origin main
